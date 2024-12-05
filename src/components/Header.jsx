29f40a7b24033ce8ea3d4f5f51fdf62ea65d177a
dungvq <dung.vuquoc0102@gmail.com>
2024-12-05 18:10:29 +0700

    add: homelayout

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<header>
+			{/* container */}
+			<div className="container">
+				{/* wrapper */}
+				<div>
+					{/* logo */}
+					<div>
+						<img src="/todo.png" alt="logo image" />
+					</div>
+				</div>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
